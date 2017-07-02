a148e6ad8f45e0ed437adfc372cde08b4b9e2c25
Keith Mifsud <mifsud.k@gmail.com>
2017-07-02 09:05:04 +0000

    Tested the testing suite

---
+++ tests/Eventful/TestCase.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of eventful
+ *
+ * @licence   Please view the Licence file supplied with this source code.
+ *
+ * @author    Keith Mifsud <http://www.keith-mifsud.com>
+ *
+ * @copyright Keith Mifsud 2017
+ *
+ * @since     1.0
+ * @version   1.0 Initial Release
+ */
+
+namespace Eventful\Test;
+
+
+/**
+ * An extensible test case.
+ */
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
---
+++ tests/Eventful/TestSuiteTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of eventful
+ *
+ * @licence   Please view the Licence file supplied with this source code.
+ *
+ * @author    Keith Mifsud <http://www.keith-mifsud.com>
+ *
+ * @copyright Keith Mifsud 2017
+ *
+ * @since     2.0
+ * @version   2.0 Second Release
+ */
+
+
+namespace Eventful\Test;
+
+/**
+ * Standard test for the testing suite.
+ */
+class TestSuiteTest extends TestCase
+{
+
+    public function testThatTestingSuiteWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
