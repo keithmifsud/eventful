4fa9e162334ef31be6fd05a72e05071faa7d24a8
Keith Mifsud <mifsud.k@gmail.com>
2017-11-17 02:48:35 +0000

    GH-8 GH-3 Added a currency code value object.

--- tests/Eventful/Unit/Model/Domain/ValueObject/CurrencyCodeValueObjectTest.php
+++ tests/Eventful/Unit/Model/Domain/ValueObject/CurrencyCodeValueObjectTest.php
@@ -14,9 +14,11 @@
 
 namespace Eventful\Test\Unit\Model\Domain\ValueObject;
 
-use Eventful\Domain\ValueObject\CurrencyCodeValueObject;
-use Eventful\Domain\ValueObject\ValueObject;
 use Eventful\Test\TestCase;
+use Eventful\Domain\ValueObject\ValueObject;
+use Eventful\Common\Exception\UndefinedEnumeratorValue;
+use Eventful\Domain\ValueObject\CurrencyCodeValueObject;
+
 
 /**
  * Tests for the currency code value object.
@@ -36,6 +38,34 @@ class CurrencyCodeValueObjectTest extends TestCase
             ValueObject::class,
             $currencyCodeValueObject
         );
+    }
+
+
+    /**
+     * Tests that it can be instantiated from a valid currency code.
+     *
+     * @test
+     */
+    public function it_can_be_instantiated_from_a_valid_currency_code()
+    {
+        $currencyCodeValueObject = new CurrencyCodeValueObject('EUR');
+        $this->assertInstanceOf(
+            CurrencyCodeValueObject::class,
+            $currencyCodeValueObject
+        );
+    }
+
+
+    /**
+     * Tests that an exception is throws when instantiated from an invalid
+     * currency code.
+     *
+     * @test
+     * @expectedException UndefinedEnumeratorValue
+     */
+    public function it_throws_exception_when_instantiated_from_an_invalid_code()
+    {
+        $invalidCode = new CurrencyCodeValueObject('INV');
     }
 }
 
